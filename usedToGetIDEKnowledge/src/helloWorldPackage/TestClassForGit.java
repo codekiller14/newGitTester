5a0c120705487688a37889be83e5f385fb6e0e0f
kajalchaudhary <kajalchaudhary@kajals-air>
2020-06-04 01:32:54 +0530

    two syso added

--- usedToGetIDEKnowledge/src/helloWorldPackage/TestClassForGit.java
+++ usedToGetIDEKnowledge/src/helloWorldPackage/TestClassForGit.java
@@ -4,6 +4,8 @@ public class TestClassForGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hey i am a git user");
+		System.out.println("i have chnges the file now");
 
 	}
 
